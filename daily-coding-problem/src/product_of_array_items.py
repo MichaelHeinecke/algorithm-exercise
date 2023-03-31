2f5480605115c527e6f7b7a1e85ee9b54e6aee09
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-03-31 16:04:59 +0200

    Solution for multiplying array items without self using no division

--- daily-coding-problem/src/product_of_array_items.py
+++ daily-coding-problem/src/product_of_array_items.py
@@ -17,3 +17,26 @@ def multiply_array_items_without_self(input_array: List[int]) -> List[int]:
     array_product = math.prod(input_array)
 
     return [int(array_product / value) for value in input_array]
+
+
+def multiply_array_items_without_self_no_division(input_array: List[int]) \
+        -> List[int]:
+    # Factors left of index i and right of index make the product for at
+    # index i. Multiplication of left and right factors will yield the product
+    # without factor at index i.
+    n = len(input_array)
+    left = [1] * n
+    right = [1] * n
+
+    # Construct the array with the factors left of index i
+    for i in range(1, n):
+        left[i] = input_array[i - 1] * left[i - 1]
+
+    # Construct the array with the factors right of index i
+    for j in range(n - 2, -1, -1):
+        right[j] = input_array[j + 1] * right[j + 1]
+
+    # Construct the array with the resulting product at index i
+    products = [lf * rf for lf, rf in zip(left, right)]
+
+    return products
--- daily-coding-problem/test/test_product_of_array_items.py
+++ daily-coding-problem/test/test_product_of_array_items.py
@@ -1,4 +1,5 @@
-from src.product_of_array_items import multiply_array_items_without_self
+from src.product_of_array_items import multiply_array_items_without_self, \
+    multiply_array_items_without_self_no_division
 
 
 def test_array_items_are_multiplied_correctly_1():
@@ -12,4 +13,18 @@ def test_array_items_are_multiplied_correctly_2():
     input_array = [3, 2, 1]
     expected_output_array = [2, 3, 6]
     assert multiply_array_items_without_self(
+        input_array) == expected_output_array
+
+
+def test_array_items_are_multiplied_correctly_no_division_1():
+    input_array = [1, 2, 3, 4, 5]
+    expected_output_array = [120, 60, 40, 30, 24]
+    assert multiply_array_items_without_self_no_division(
+        input_array) == expected_output_array
+
+
+def test_array_items_are_multiplied_correctly_no_division_2():
+    input_array = [3, 2, 1]
+    expected_output_array = [2, 3, 6]
+    assert multiply_array_items_without_self_no_division(
         input_array) == expected_output_array
