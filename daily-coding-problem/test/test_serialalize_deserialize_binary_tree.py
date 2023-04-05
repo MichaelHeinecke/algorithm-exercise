c92c51e6a8af1a276e57db264cb3e1da4ef96bb0
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-04-05 11:46:53 +0200

    Refactor code with BinaryTree class wrapping (de)serialize methods

--- daily-coding-problem/test/test_serialalize_deserialize_binary_tree.py
+++ daily-coding-problem/test/test_serialalize_deserialize_binary_tree.py
@@ -1,25 +1,28 @@
 import pytest
 
-from src.serialalize_deserialize_binary_tree import Node, deserialize, \
-    serialize
+from src.serialalize_deserialize_binary_tree import Node, BinaryTree
 
 
 @pytest.fixture
-def tree():
-    return Node("root", Node("left", Node("left.left")), Node("right"))
+def tree() -> BinaryTree:
+    binary_tree = BinaryTree()
+    binary_tree.root = Node("root")
+    binary_tree.root.left = Node("left")
+    binary_tree.root.left.left = Node("left.left")
+    binary_tree.root.right = Node("right")
+    return binary_tree
 
 
 @pytest.fixture
 def serialized_tree():
-    return "root left left.left no_child no_child no_child right " \
-           "no_child no_child"
+    return "root left left.left no_child no_child no_child right no_child no_child"
 
 
-def test_deserializing_binary_tree_returns_expected_tree(serialized_tree):
-    deserialized_tree = deserialize(serialized_tree)
+def test_deserializing_binary_tree_returns_expected_tree(tree, serialized_tree):
+    deserialized_tree = tree.deserialize(serialized_tree)
     assert deserialized_tree.left.left.val == "left.left"
 
 
 def test_serialize_tree_returns_expected_string(tree, serialized_tree):
-    output_string = serialize(tree)
+    output_string = tree.serialize()
     assert output_string == serialized_tree
